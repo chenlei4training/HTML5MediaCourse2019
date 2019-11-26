8e768c11eb12b46f5d7e567cae2c8e8658795216
梦宇 <xtiqin@163.com>
2019-11-26 17:58:48 +0800

    忽略不必要的文件

---
+++ MyClassroom/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        teacher: path.join(__dirname, "FrontProjects", "Teacher", "src", "main.js"),
+        student: path.join(__dirname, "FrontProjects", "Student", "src", "main.js")
+    },
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "[name].js"
+    }
+};
